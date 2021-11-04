83a0109ed867d0e4f926e93a5be7dfe191ec0bef
Salah Eddine Ait Blakacem <ab.salah012@gmail.com>
2021-11-04 11:20:26 +0100

    feat: add initial

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
+import theme from '../lib/theme'
+import '@fontsource/comfortaa'
+import '@fontsource/nunito'
+import '@fontsource/handlee'
+
+// import { AnimatePresence } from 'framer-motion'
+
+function Website({ Component, pageProps, router }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Layout router={router}>
+        {/* <AnimatePresence exitBeforeEnter initial={true}> */}
+        <Component {...pageProps} key={router.route} />
+        {/* </AnimatePresence> */}
+      </Layout>
+    </ChakraProvider>
+  )
+}
+
+export default Website
---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Intro from '../components/sections/intro'
+import Projects from '../components/sections/projects'
+
+const Index = () => {
+  return (
+    <React.Fragment>
+      <Intro />
+      <Projects />
+    </React.Fragment>
+  )
+}
+
+export default Index
