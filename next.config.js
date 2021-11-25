8cccfebefbe129891d5952cf55fbc0264a986440
Salah Eddine Ait Blakacem <ab.salah012@gmail.com>
2021-11-25 01:45:50 +0100

    add bundleAnalyzer

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 const withYaml = require('next-plugin-yaml')
-module.exports = withYaml({
-  reactStrictMode: true
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true'
 })
+
+module.exports = {
+  webpack(config) {
+    config.plugins = config.plugins.filter(
+      p => p.constructor.name !== 'UglifyJsPlugin'
+    )
+    config.plugins.push(new UglifyJsPlugin())
+    return config
+  }
+}
+
+module.exports = withYaml(module.exports)
+
+module.exports = withBundleAnalyzer(module.exports)
