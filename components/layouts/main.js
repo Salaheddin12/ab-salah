114308d3885168aec4d45cf9c2f82817a2aa76e3
Salah Eddine Ait Blakacem <ab.salah012@gmail.com>
2021-12-17 11:49:36 +0100

    feat: add custom 404 page

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -5,7 +5,7 @@ import Footer from '../footer'
 
 const Main = ({ children, router }) => {
   return (
-    <Box as="main" pb={8}>
+    <Box as="main" pb={8} minH={'100vh'}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <meta name="description" content="Salah's homepage" /> */}
---
+++ pages/404.js
@@ -0,0 +1,31 @@
+import Image from 'next/image'
+import Section from '../components/section'
+import { Box, Heading } from '@chakra-ui/react'
+
+export default function Custom404() {
+  return (
+    <Section
+      className="section"
+      delay={0.1}
+      display="flex"
+      flexDirection={'row'}
+      mb={{ base: 0 }}
+    >
+      <Image alt="404 image" src={'/images/404.svg'} width={518} height={518} />
+      <Box display={'flex'} flexDirection={'column'} justifyContent={'center'}>
+        <Heading
+          as="h2"
+          color={'brandRed'}
+          variant="section-title"
+          pb="2rem"
+          margin={0}
+        >
+          Whoops!
+        </Heading>
+        <Heading as="h4" color={'darkgrey'}>
+          This page got lost in conversation.
+        </Heading>
+      </Box>
+    </Section>
+  )
+}
